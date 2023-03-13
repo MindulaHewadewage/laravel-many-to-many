e5acbc2f855f9f8d07fb96af383f1a8841cfdcdf
MindulaHewadewage <mindufernando@gmail.com>
2023-03-13 16:51:38 +0100

    tech seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,23 @@ class TechnologySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $technologies = [
+            ['label' => 'HTML', 'color' => 'primary'],
+            ['label' => 'CSS', 'color' => 'success'],
+            ['label' => 'JS', 'color' => 'danger'],
+            ['label' => 'Bootstrap', 'color' => 'warning'],
+            ['label' => 'Vue', 'color' => 'info'],
+            ['label' => 'SQL', 'color' => 'indigo'],
+            ['label' => 'PHP', 'color' => 'primary'],
+            ['label' => 'Laravel', 'color' => 'teal']
+        ];
+
+        foreach ($technologies as $technology) {
+            $new_technology = new Technology();
+            $new_technology->label = $technology['label'];
+            $new_technology->color = $technology['color'];
+
+            $new_technology->save();
+        }
     }
 }
